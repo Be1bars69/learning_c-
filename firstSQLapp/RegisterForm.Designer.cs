85c395bd1e3f90094ea368d9725facab4f1e1e20
Be1bars69 <murataly.beibars@gmail.com>
2024-04-20 14:29:38 +0500

    version 1

---
+++ firstSQLapp/RegisterForm.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace firstSQLapp
+{
+    partial class RegisterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.userSurnameField = new System.Windows.Forms.TextBox();
+            this.userNameField = new System.Windows.Forms.TextBox();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.passField = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.loginField = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.closeButton = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.loginForm = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(145)))), ((int)(((byte)(32)))), ((int)(((byte)(7)))));
+            this.panel1.Controls.Add(this.loginForm);
+            this.panel1.Controls.Add(this.userSurnameField);
+            this.panel1.Controls.Add(this.userNameField);
+            this.panel1.Controls.Add(this.buttonRegister);
+            this.panel1.Controls.Add(this.passField);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.loginField);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 1;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // userSurnameField
+            // 
+            this.userSurnameField.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.userSurnameField.Location = new System.Drawing.Point(488, 139);
+            this.userSurnameField.Multiline = true;
+            this.userSurnameField.Name = "userSurnameField";
+            this.userSurnameField.Size = new System.Drawing.Size(300, 64);
+            this.userSurnameField.TabIndex = 7;
+            this.userSurnameField.Enter += new System.EventHandler(this.userSurnameField_Enter);
+            this.userSurnameField.Leave += new System.EventHandler(this.userSurnameField_Leave);
+            // 
+            // userNameField
+            // 
+            this.userNameField.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.userNameField.Location = new System.Drawing.Point(83, 139);
+            this.userNameField.Multiline = true;
+            this.userNameField.Name = "userNameField";
+            this.userNameField.Size = new System.Drawing.Size(300, 64);
+            this.userNameField.TabIndex = 6;
+            this.userNameField.Enter += new System.EventHandler(this.userNameField_Enter);
+            this.userNameField.Leave += new System.EventHandler(this.userNameField_Leave);
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(127)))), ((int)(((byte)(9)))));
+            this.buttonRegister.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonRegister.FlatAppearance.BorderSize = 0;
+            this.buttonRegister.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(57)))), ((int)(((byte)(3)))));
+            this.buttonRegister.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(152)))), ((int)(((byte)(2)))));
+            this.buttonRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonRegister.ForeColor = System.Drawing.Color.White;
+            this.buttonRegister.Location = new System.Drawing.Point(238, 352);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(324, 61);
+            this.buttonRegister.TabIndex = 5;
+            this.buttonRegister.Text = "Зарегестрироваться";
+            this.buttonRegister.UseVisualStyleBackColor = false;
+            this.buttonRegister.Click += new System.EventHandler(this.buttonRegister_Click);
+            // 
+            // passField
+            // 
+            this.passField.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passField.Location = new System.Drawing.Point(488, 254);
+            this.passField.Name = "passField";
+            this.passField.Size = new System.Drawing.Size(300, 53);
+            this.passField.TabIndex = 4;
+            this.passField.UseSystemPasswordChar = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::firstSQLapp.Properties.Resources._lock;
+            this.pictureBox2.Location = new System.Drawing.Point(418, 243);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // loginField
+            // 
+            this.loginField.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginField.Location = new System.Drawing.Point(83, 243);
+            this.loginField.Multiline = true;
+            this.loginField.Name = "loginField";
+            this.loginField.Size = new System.Drawing.Size(300, 64);
+            this.loginField.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::firstSQLapp.Properties.Resources.user;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 243);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(29)))), ((int)(((byte)(127)))));
+            this.panel2.Controls.Add(this.closeButton);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 109);
+            this.panel2.TabIndex = 0;
+            // 
+            // closeButton
+            // 
+            this.closeButton.AutoSize = true;
+            this.closeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.closeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.closeButton.ForeColor = System.Drawing.Color.White;
+            this.closeButton.Location = new System.Drawing.Point(770, 0);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(30, 36);
+            this.closeButton.TabIndex = 1;
+            this.closeButton.Text = "x";
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 31.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(239)))), ((int)(((byte)(239)))));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(800, 109);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Регистрация";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // loginForm
+            // 
+            this.loginForm.AutoSize = true;
+            this.loginForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginForm.ForeColor = System.Drawing.Color.White;
+            this.loginForm.Location = new System.Drawing.Point(331, 423);
+            this.loginForm.Name = "loginForm";
+            this.loginForm.Size = new System.Drawing.Size(135, 18);
+            this.loginForm.TabIndex = 8;
+            this.loginForm.Text = "Авторизоваться";
+            this.loginForm.Click += new System.EventHandler(this.loginForm_Click);
+            // 
+            // RegisterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "RegisterForm";
+            this.Text = "RegisterForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonRegister;
+        private System.Windows.Forms.TextBox passField;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox loginField;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label closeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userSurnameField;
+        private System.Windows.Forms.TextBox userNameField;
+        private System.Windows.Forms.Label loginForm;
+    }
+}
---
+++ firstSQLapp/RegisterForm.cs
@@ -0,0 +1,160 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace firstSQLapp
+{
+    public partial class RegisterForm : Form
+    {
+        public RegisterForm()
+        {
+            InitializeComponent();
+
+            userNameField.Text = "Ввидите имя";
+            userNameField.ForeColor = Color.Gray;
+
+            userSurnameField.Text = "Ввидите фамилию";
+            userSurnameField.ForeColor = Color.Gray;
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        Point lastPoint;
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void userNameField_Enter(object sender, EventArgs e)
+        {
+            if (userNameField.Text == "Ввидите имя")
+            {
+                userNameField.Text = "";
+                userNameField.ForeColor = Color.Black;
+            }
+        }
+
+        private void userNameField_Leave(object sender, EventArgs e)
+        {
+            if (userNameField.Text == "")
+            {
+                userNameField.Text = "Ввидите имя";
+                userNameField.ForeColor = Color.Gray;
+            }
+        }
+
+        private void userSurnameField_Enter(object sender, EventArgs e)
+        {
+            if (userSurnameField.Text == "Ввидите фамилию") 
+            {
+                userSurnameField.Text = "";
+                userSurnameField.ForeColor = Color.Black;
+            }
+        }
+
+        private void userSurnameField_Leave(object sender, EventArgs e)
+        {
+            if (userSurnameField.Text == "") 
+            {
+                userSurnameField.Text = "Ввидите фамилию";
+                userSurnameField.ForeColor = Color.Black;
+            }
+        }
+
+        private void buttonRegister_Click(object sender, EventArgs e)
+        {
+            if (userNameField.Text == "Ввидите имя") 
+            {
+                MessageBox.Show("Ввидите имя");
+                return;
+            }
+            if (userSurnameField.Text == "Ввидите фамилию")
+            {
+                MessageBox.Show("Ввидите фамилию");
+                return;
+            }
+            if (loginField.Text == "")
+            {
+                MessageBox.Show("Ввидите логин");
+                return;
+            }
+            if (passField.Text == "")
+            {
+                MessageBox.Show("Ввидите пароль");
+                return;
+            }
+
+            if(isUserExists())
+                return;
+
+            DB db = new DB();
+            MySqlCommand command = new MySqlCommand("INSERT INTO `users` (`login`, `pass`, `name`, `surname`) VALUES (@login, @pass, @name, @surname)", db.getConnection());
+            command.Parameters.Add("@login", MySqlDbType.VarChar).Value = loginField.Text;
+            command.Parameters.Add("@pass", MySqlDbType.VarChar).Value = passField.Text;
+            command.Parameters.Add("@name", MySqlDbType.VarChar).Value = userNameField.Text;
+            command.Parameters.Add("@surname", MySqlDbType.VarChar).Value = userSurnameField.Text;
+
+            db.openConnection();
+
+            if (command.ExecuteNonQuery() == 1)
+                MessageBox.Show("Аккаунт был создан");
+            else
+                MessageBox.Show("Аккаунт не был создан");
+
+
+            db.closeConnection();
+        }
+
+
+
+        public Boolean isUserExists() 
+        {
+            DB db = new DB();
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `users` WHERE `login` = @uL", db.getConnection());
+            command.Parameters.Add("@uL", MySqlDbType.VarChar).Value = loginField.Text;
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0) 
+            {
+                MessageBox.Show("Такой логин уже есть, ввидите другой");
+                return true;
+            }
+            else
+                return false;
+        }
+
+        private void loginForm_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+        }
+    }
+
+}
